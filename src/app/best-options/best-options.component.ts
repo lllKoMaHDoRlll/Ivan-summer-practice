4c91c4d37440089acf367851056a487b19d7e857
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-15 20:42:40 +0300

    generate best options component

---
+++ src/app/best-options/best-options.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-best-options',
+  standalone: true,
+  imports: [],
+  templateUrl: './best-options.component.html',
+  styleUrl: './best-options.component.css'
+})
+export class BestOptionsComponent {
+
+}
