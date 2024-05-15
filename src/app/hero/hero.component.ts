f4b3d38bfd15f5a694dbe8692610d10ff1302cef
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-15 15:30:22 +0300

    implement hero component's layout

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { BookingSearchComponent } from '../booking-search/booking-search.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    BookingSearchComponent
+  ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
