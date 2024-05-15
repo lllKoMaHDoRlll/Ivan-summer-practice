6899988fe6388cac51dfa209f7db2ae732fb8a40
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-15 13:55:41 +0300

    implement header component's layout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faTelegram, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  faPhone = faPhone;
+  faTelegram = faTelegram;
+  faInstagram = faInstagram;
 }
