b93c064d7a139ad09f2691cebb7602f5801121ac
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-15 20:08:13 +0300

    implement city offers card component's layout

--- src/app/city-offers-card/city-offers-card.component.ts
+++ src/app/city-offers-card/city-offers-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CityOffersData } from '../city-offers-data';
 
 @Component({
   selector: 'app-city-offers-card',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
   templateUrl: './city-offers-card.component.html',
   styleUrl: './city-offers-card.component.css'
 })
-export class CityOffersCardComponent {
 
+export class CityOffersCardComponent {
+  @Input() city_offers_data!: CityOffersData;
+  image_base_filepath = "/assets/images/cities/";
 }
