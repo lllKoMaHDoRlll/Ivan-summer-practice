fc97ab0e3d356651b5eebbddb1ca0dd62ae8f897
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-15 20:21:54 +0300

    generate component company advantages

---
+++ src/app/company-advantages/company-advantages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-advantages',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-advantages.component.html',
+  styleUrl: './company-advantages.component.css'
+})
+export class CompanyAdvantagesComponent {
+
+}
