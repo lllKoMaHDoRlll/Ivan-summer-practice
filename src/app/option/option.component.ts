73e6110cfc4305687d8216bd33d16216420dbfc4
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-15 21:34:14 +0300

    implement option component's layout

--- src/app/option/option.component.ts
+++ src/app/option/option.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { OptionData } from '../option-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-option',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './option.component.html',
   styleUrl: './option.component.css'
 })
-export class OptionComponent {
 
+export class OptionComponent {
+  image_base_filepath = "assets/images/cities/";
+  @Input() option_data!: OptionData;
 }
