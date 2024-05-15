03047db188e61adedca04373c862c569703f2f23
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-15 13:31:55 +0300

    generate component booking search

---
+++ src/app/booking-search/booking-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './booking-search.component.html',
+  styleUrl: './booking-search.component.css'
+})
+export class BookingSearchComponent {
+
+}
