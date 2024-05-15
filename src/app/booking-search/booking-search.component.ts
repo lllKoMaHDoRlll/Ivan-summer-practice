abc1252d6f8a88414d553a38f28737ec8118bbbe
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-15 15:29:43 +0300

    implement booking search component's layout

--- src/app/booking-search/booking-search.component.ts
+++ src/app/booking-search/booking-search.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLocationDot, faCalendarDays, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-booking-search',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './booking-search.component.html',
   styleUrl: './booking-search.component.css'
 })
 export class BookingSearchComponent {
-
+  faLocationDot = faLocationDot;
+  faCalendarDays = faCalendarDays;
+  faUser = faUser;
 }
