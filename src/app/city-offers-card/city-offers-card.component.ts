da1897998f5e3ed4af18a154cfecdd2d0673fc88
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-15 17:18:35 +0300

    generate component city offers card

---
+++ src/app/city-offers-card/city-offers-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-offers-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './city-offers-card.component.html',
+  styleUrl: './city-offers-card.component.css'
+})
+export class CityOffersCardComponent {
+
+}
