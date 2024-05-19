e856882687958f7a57ccf8a49d792a46cc7d00ca
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-19 17:53:42 +0300

    implement catalog component's layout

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,12 +1,115 @@
 import { Component } from '@angular/core';
+import { PlaceCardComponent } from '../place-card/place-card.component';
+import { PlaceCardData } from '../place-card-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    PlaceCardComponent
+  ],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
 export class CatalogComponent {
-
+  place_cards_data: PlaceCardData[] = [
+    {
+      image_filename: "kislovodsk.png",
+      place_name: "отель «солнечный»",
+      address: "г. Пятигорск, ул. Горького, 115",
+      treatment_profiles: [
+        "Сердечно-сосудистая система",
+        "Желудочно-кишечный тракт (ЖКТ)",
+        "Обмен веществ",
+        "Гинекология",
+        "Кровообращение",
+        "Заболевания печени",
+        "Детокс",
+        "Похудение"
+      ],
+      min_price: 9500
+    },
+    {
+      image_filename: "pyatigorsk.png",
+      place_name: "база отдыха «база»",
+      address: "г. Пятигорск, ул. Горького, 115",
+      treatment_profiles: [
+        "Сердечно-сосудистая система",
+        "Желудочно-кишечный тракт (ЖКТ)",
+        "Обмен веществ",
+        "Гинекология",
+        "Кровообращение",
+        "Заболевания печени",
+        "Детокс",
+        "Похудение"
+      ],
+      min_price: 9500
+    },
+    {
+      image_filename: "jeleznovodsk.png",
+      place_name: "санаторий «Resort»",
+      address: "г. Пятигорск, ул. Горького, 115",
+      treatment_profiles: [
+        "Сердечно-сосудистая система",
+        "Желудочно-кишечный тракт (ЖКТ)",
+        "Обмен веществ",
+        "Гинекология",
+        "Кровообращение",
+        "Заболевания печени",
+        "Детокс",
+        "Похудение"
+      ],
+      min_price: 9500
+    },
+    {
+      image_filename: "essentuki.png",
+      place_name: "отель «солнечный»",
+      address: "г. Пятигорск, ул. Горького, 115",
+      treatment_profiles: [
+        "Сердечно-сосудистая система",
+        "Желудочно-кишечный тракт (ЖКТ)",
+        "Обмен веществ",
+        "Гинекология",
+        "Кровообращение",
+        "Заболевания печени",
+        "Детокс",
+        "Похудение"
+      ],
+      min_price: 9500
+    },
+    {
+      image_filename: "kislovodsk.png",
+      place_name: "база отдыха «база»",
+      address: "г. Пятигорск, ул. Горького, 115",
+      treatment_profiles: [
+        "Сердечно-сосудистая система",
+        "Желудочно-кишечный тракт (ЖКТ)",
+        "Обмен веществ",
+        "Гинекология",
+        "Кровообращение",
+        "Заболевания печени",
+        "Детокс",
+        "Похудение"
+      ],
+      min_price: 9500
+    },
+    {
+      image_filename: "pyatigorsk.png",
+      place_name: "санаторий «Resort»",
+      address: "г. Пятигорск, ул. Горького, 115",
+      treatment_profiles: [
+        "Сердечно-сосудистая система",
+        "Желудочно-кишечный тракт (ЖКТ)",
+        "Обмен веществ",
+        "Гинекология",
+        "Кровообращение",
+        "Заболевания печени",
+        "Детокс",
+        "Похудение"
+      ],
+      min_price: 9500
+    }
+  ];
 }
