efa3e5c88a3cc87cdfb24180a779ab792e04a842
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-19 18:48:12 +0300

    implement footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faTelegram, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faPhone = faPhone;
+  faTelegram = faTelegram;
+  faInstagram = faInstagram;
 }
