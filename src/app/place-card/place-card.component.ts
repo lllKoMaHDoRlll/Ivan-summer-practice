80ba4f253161683ff8c473e2f24bfe49db6c642c
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-19 17:53:14 +0300

    implement place card component's layout

--- src/app/place-card/place-card.component.ts
+++ src/app/place-card/place-card.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PlaceCardData } from '../place-card-data';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faWifi, faUtensils, faUmbrellaBeach } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-place-card',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './place-card.component.html',
   styleUrl: './place-card.component.css'
 })
 export class PlaceCardComponent {
+  image_base_filepath = "assets/images/cities/";
+  @Input() place_card_data!: PlaceCardData;
 
+  faWifi = faWifi;
+  faUtensils = faUtensils;
+  faUmbrellaBeach = faUmbrellaBeach;
 }
