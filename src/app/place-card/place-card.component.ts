3f87d0b5b3bb97b25fd6de2dc3cdd0172802dcb6
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-19 15:58:09 +0300

    generate component place card

---
+++ src/app/place-card/place-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './place-card.component.html',
+  styleUrl: './place-card.component.css'
+})
+export class PlaceCardComponent {
+
+}
