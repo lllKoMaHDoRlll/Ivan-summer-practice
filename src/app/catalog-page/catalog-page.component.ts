596fff5797669929c262d34fccca97b374e732df
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-24 20:28:08 +0300

    implement catalog-page component

--- src/app/catalog-page/catalog-page.component.ts
+++ src/app/catalog-page/catalog-page.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../hero/hero.component';
+import { CatalogComponent } from '../catalog/catalog.component';
+import { BestOptionsComponent } from '../best-options/best-options.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-catalog-page',
   standalone: true,
-  imports: [],
+  imports: [
+    HeroComponent,
+    CatalogComponent,
+    BestOptionsComponent,
+    FooterComponent
+  ],
   templateUrl: './catalog-page.component.html',
   styleUrl: './catalog-page.component.css'
 })
