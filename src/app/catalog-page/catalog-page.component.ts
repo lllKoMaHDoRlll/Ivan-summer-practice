be8b9ee08439c8047a83c3570ff189b498b78894
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-24 19:29:35 +0300

    generate component catalog page

---
+++ src/app/catalog-page/catalog-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalog-page.component.html',
+  styleUrl: './catalog-page.component.css'
+})
+export class CatalogPageComponent {
+
+}
