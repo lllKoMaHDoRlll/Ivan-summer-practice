22bb6045353ca58f3740b95a6ee49ac35ea26077
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-24 19:30:52 +0300

    implement routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogPageComponent } from './catalog-page/catalog-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home Page'
+  },
+  {
+    path: 'catalog',
+    component: CatalogPageComponent,
+    title: 'Catalog Page'
+  }
+];
