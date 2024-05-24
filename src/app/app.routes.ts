8bb4ecc06f7af4c89b7c065cbbbe9f185058113a
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-24 20:31:36 +0300

    add news page to router config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogPageComponent } from './catalog-page/catalog-page.component';
+import { NewsPageComponent } from './news-page/news-page.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: 'catalog',
     component: CatalogPageComponent,
     title: 'Catalog Page'
+  },
+  {
+    path: 'news',
+    component: NewsPageComponent,
+    title: 'News Page'
   }
 ];
