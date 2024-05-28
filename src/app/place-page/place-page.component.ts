e2331a1bd4fb2d701defdcd73b0be009ddfbb6f7
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-28 22:13:00 +0300

    generate place page component

---
+++ src/app/place-page/place-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './place-page.component.html',
+  styleUrl: './place-page.component.css'
+})
+export class PlacePageComponent {
+
+}
