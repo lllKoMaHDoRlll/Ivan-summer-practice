298d23d0786642312955cc349064f0990e85e65a
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-28 09:57:50 +0300

    generate news catalog card component

---
+++ src/app/news-catalog-card/news-catalog-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-catalog-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './news-catalog-card.component.html',
+  styleUrl: './news-catalog-card.component.css'
+})
+export class NewsCatalogCardComponent {
+
+}
