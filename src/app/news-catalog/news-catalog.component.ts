1b2b5c9b811c8ba3c01390dd54cc4929b55b94b2
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-28 09:57:34 +0300

    generate news catalog component

---
+++ src/app/news-catalog/news-catalog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-catalog',
+  standalone: true,
+  imports: [],
+  templateUrl: './news-catalog.component.html',
+  styleUrl: './news-catalog.component.css'
+})
+export class NewsCatalogComponent {
+
+}
