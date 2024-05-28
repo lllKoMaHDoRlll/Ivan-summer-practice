44e40e19a79276f84f5d2dca28547a5da581649e
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-28 22:13:12 +0300

    generate place page card component

---
+++ src/app/place-page-card/place-page-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place-page-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './place-page-card.component.html',
+  styleUrl: './place-page-card.component.css'
+})
+export class PlacePageCardComponent {
+
+}
