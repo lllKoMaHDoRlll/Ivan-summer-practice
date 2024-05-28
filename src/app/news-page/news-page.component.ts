6c36d87876972b39051451e8add79a254d625bdb
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-28 10:49:36 +0300

    implement news page component

--- src/app/news-page/news-page.component.ts
+++ src/app/news-page/news-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../hero/hero.component';
+import { FooterComponent } from '../footer/footer.component';
+import { NewsCatalogComponent } from '../news-catalog/news-catalog.component';
 
 @Component({
   selector: 'app-news-page',
   standalone: true,
-  imports: [],
+  imports: [
+    HeroComponent,
+    FooterComponent,
+    NewsCatalogComponent
+  ],
   templateUrl: './news-page.component.html',
   styleUrl: './news-page.component.css'
 })
