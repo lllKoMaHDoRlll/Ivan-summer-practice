481d19ff04741c764425b74ae583faa083b80681
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-31 15:14:45 +0300

    implement place page component

--- src/app/place-page/place-page.component.ts
+++ src/app/place-page/place-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../hero/hero.component';
+import { FooterComponent } from '../footer/footer.component';
+import { PlacePageCardComponent } from '../place-page-card/place-page-card.component';
 
 @Component({
   selector: 'app-place-page',
   standalone: true,
-  imports: [],
+  imports: [
+    HeroComponent,
+    FooterComponent,
+    PlacePageCardComponent
+  ],
   templateUrl: './place-page.component.html',
   styleUrl: './place-page.component.css'
 })
