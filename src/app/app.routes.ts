71ba64544b8613a982dec1a5480efb3b9e0ca369
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-31 15:17:06 +0300

    add article page to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { CatalogPageComponent } from './catalog-page/catalog-page.component';
 import { NewsPageComponent } from './news-page/news-page.component';
 import { PlacePageComponent } from './place-page/place-page.component';
+import { ArticlePageComponent } from './article-page/article-page.component';
 
 export const routes: Routes = [
   {
@@ -24,5 +25,10 @@ export const routes: Routes = [
     path: 'place',
     component: PlacePageComponent,
     title: 'Place'
+  },
+  {
+    path: 'article',
+    component: ArticlePageComponent,
+    title: 'Article'
   }
 ];
