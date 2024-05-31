1813f56b732ba1d7b1db9899eae11d9895cade7d
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-05-31 15:38:36 +0300

    implement article page component

--- src/app/article-page/article-page.component.ts
+++ src/app/article-page/article-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../hero/hero.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-article-page',
   standalone: true,
-  imports: [],
+  imports: [
+    HeroComponent,
+    FooterComponent
+  ],
   templateUrl: './article-page.component.html',
   styleUrl: './article-page.component.css'
 })
